971eeebb9f54f65dce76ac3671b98ec58eb41f86
Sam Bardsley <93604147+SamBardsley@users.noreply.github.com>
2022-06-02 15:36:25 +0000

    Worked on outline

--- Wordle.java
+++ Wordle.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 class Wordle {
   public static void main(String[] args) {
     System.out.println("Hello worldle!");
+    Scanner scn = new Scanner(System.in);
+    String guess = scn.nextLine();
+    verifier(guess);
+  }
+
+  public static int[] verifier(String guess) {
+    int[] correctness = new int[3];
+    return correctness;
   }
 }
