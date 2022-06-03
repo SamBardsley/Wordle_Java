b626a357af177243821f9e48fb50fee5f7cb750f
Sam Bardsley <93604147+SamBardsley@users.noreply.github.com>
2022-06-03 15:24:48 +0000

    Added outline for List object

--- List.java
+++ List.java
@@ -1,2 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class List {
+  
+  public List() {
+    File allWords = new File("words.txt");
+  }
+}
+
 // needs getRandom method, and one with just a massive list
 // in the future we will maybe verify every word depending on how long that takes
