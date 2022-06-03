e90b1c8812b87bbf9c1e386d99c0f4732c889a89
Sam Bardsley <93604147+SamBardsley@users.noreply.github.com>
2022-06-03 15:42:15 +0000

    Added list

--- Wordle.java
+++ Wordle.java
@@ -7,6 +7,7 @@ class Wordle {
   public static final String answer = "crane";
   
   public static void main(String[] args) {
+    List words = new List();
 
     int tries = 0;
     for(int i = 0; i < 5; i++) {
