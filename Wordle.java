aa8269483cec8b66264f8cbb1f45fa1a2c944593
Jack McIver <jacksonmciver11@gmail.com>
2022-06-03 07:07:42 +0000

    Added unique messages when user guesses word correctly; added to-do list

--- Wordle.java
+++ Wordle.java
@@ -3,16 +3,23 @@ import java.util.Scanner;
 
 class Wordle {
   public static final Scanner input = new Scanner(System.in);
+  public static final String[] message = {"Genius.", "Magnificent.", "Impressive.", "Splendid.", "Great.", "Phew."};
   public static final String answer = "crane";
   
   public static void main(String[] args) {
 
+    int tries = 0;
     for(int i = 0; i < 5; i++) {
       int[] score = correctness(); 
       System.out.println(Arrays.toString(score));
+      tries++;
       if (verifier(score)) break;
     }
-    System.out.println("You're done.");
+    if(tries == 1) {
+      System.out.println(message[0] + " You guessed the Wordle in 1 try.");
+    } else {
+      System.out.println(message[tries] + " You guessed the Wordle in " + tries + " tries.");
+    }
   }
 
   // for loop runs through string 'b' and compares it to indexes of string 'a'
