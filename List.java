1c8111942b078266da36861d68040319b3520c5b
Michael <93603935+Gedgefork-Michael@users.noreply.github.com>
2022-06-14 15:36:25 +0000

    Replaced current String words with the guessablewords

--- List.java
+++ List.java
@@ -12,7 +12,7 @@ public class List {
   }
 
 
-static String words = "women nikau swack feens fyles poled clags starn bindi woops fanos cabin souct trass shoat lefty durra hypes junta baisa bises kipps sable abacs thurl nurrs saris wroth venal texas soman linds laden nolos pixie calms chert oxbow groma nomen potae noyed fifty emerg shtup aspic shone junky louns babka roton abaft hykes nipas inbye kaing pukus muils snowy piled brook avens baiza edger fawns genii mavis argal assay cocas shash wrath thins karat tunny mudge syped chose zupas hants leech lyric winds mened momus usher qophs ombus gavel swive slant firns beigy unlid flegs wangs awner claut ceded manos fuggy bunde shute snoke bulky cents agama chess ranid flurr dewar night porks voema cimex samfu query snipy glens kests peril falls urges krunk tased folia orgia verve rinks choko hully fakey durgy polje sects giant iftar hayed elfed likes sword banty blech daubs exies tetra agros shier kines yanks";
+static String words = "women death cabin words among ready truth false ashes sonic react wrath fifty reach chose chase slash faked snake sammy lyric zebra gavel break broke brave quest crack bulky cents night girly awake manly corps audio catch clout sword irony raise venom hired drawn oddly extra under greet dweeb fewer paris amaze glass cited bleak north south notes needs forum power wrong";
 
 
 
@@ -20,6 +20,3 @@ static String words = "women nikau swack feens fyles poled clags starn bindi woo
 
   
 }
-
-// needs getRandom method, and one with just a massive list
-// in the future, will maybe verify every word depending on how long that takes  
